622be187ad97bba57ab28fccc0877f55f59a346e
CarlosTeixeira <carlosteixeira@quantexa.com>
2020-12-17 19:11:05 +0000

    Day-3, Part II

--- day-3/src/lib.rs
+++ day-3/src/lib.rs
@@ -1,3 +1,17 @@
+pub struct Slope {
+    right: u8,
+    down: u8
+}
+
+impl Slope {
+    pub fn new(right: u8, down: u8) -> Slope {
+        Slope {
+           right,
+           down
+        }
+    }
+}
+
 struct TreeLine<'y> {
     line: &'y str,
     origin_size: u16,
@@ -24,53 +38,63 @@ impl TreeLine<'_> {
     }
 }
 
-pub fn number_trees(map: &str) -> u16 {
-    let lines: Vec<TreeLine> = map.lines().map(|line| TreeLine::new(line)).collect();
+pub fn number_trees(map: &str, slope: Slope) -> u16 {
+    let lines = map.lines().map(|line| TreeLine::new(line));
     fn internal(
         remaining_lines: &[TreeLine],
         num_trees: u16,
         current_pos: u16,
+        right: u16
     ) -> u16 {
         if remaining_lines.is_empty() {
             num_trees
         } else {
             let (current_line, remaining) = remaining_lines.split_at(1);
             match current_line.get(0).unwrap().tree_at(current_pos) {
-                true => internal(remaining, num_trees + 1, current_pos + 3),
-                false => internal(remaining, num_trees, current_pos + 3)
+                true => internal(remaining, num_trees + 1, current_pos + right, right),
+                false => internal(remaining, num_trees, current_pos + right, right)
             }
         }
     }
-    internal(lines.as_slice(), 0, 0)
+    let lines_that_matter = lines.enumerate().filter(|elem|{
+        (elem.0 + 1) % (slope.down as usize) == 0 && elem.0 + 1 != 1
+    }).map(|p| p.1);
+
+    let lines_that_matter: Vec<TreeLine> = lines_that_matter.collect();
+    internal(lines_that_matter.as_slice(), 0, slope.right as u16, slope.right as u16)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::number_trees;
+    use crate::{number_trees, Slope};
     use indoc::indoc;
 
     #[test]
     fn these_2_lines_work() {
+        let slope: Slope = Slope::new(3, 1);
+
         let input: &str = indoc! {
         "..##.........##.........##.........##.........##.........##.......
          #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#.."
          };
 
-        assert_eq!(number_trees(input), 0)
+        assert_eq!(number_trees(input, slope), 0)
     }
 
     #[test]
     fn only_diagonal_positions_count() {
+        let slope: Slope = Slope::new(3, 1);
         let input: &str = indoc! {
         "..##......
          #.###...#."
          };
 
-        assert_eq!(number_trees(input), 1)
+        assert_eq!(number_trees(input, slope), 1)
     }
 
     #[test]
     fn lines_can_shift() {
+        let slope: Slope = Slope::new(3, 1);
         let input: &str = indoc! {
         "..#..#.
          #...#..
@@ -78,11 +102,12 @@ mod tests {
          ....#.."
          };
 
-        assert_eq!(number_trees(input), 1)
+        assert_eq!(number_trees(input, slope), 1)
     }
 
     #[test]
     fn we_can_hit_all_trees() {
+        let slope: Slope = Slope::new(3, 1);
         let input: &str = indoc! {
         ".####
          #####
@@ -95,11 +120,12 @@ mod tests {
          #####"
          };
 
-        assert_eq!(number_trees(input), 8)
+        assert_eq!(number_trees(input, slope), 8)
     }
 
     #[test]
     fn it_works() {
+        let slope: Slope = Slope::new(3, 1);
         let input: &str = indoc! {
         "..##.........##.........##.........##.........##.........##.......
          #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
@@ -114,6 +140,26 @@ mod tests {
          .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"
          };
 
-        assert_eq!(number_trees(input), 7)
+        assert_eq!(number_trees(input, slope), 7)
+    }
+
+    #[test]
+    fn two_jumps_down_works() {
+        let slope: Slope = Slope::new(1, 2);
+        let input: &str = indoc! {
+        "..##.........##.........##.........##.........##.........##.......
+         #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+         .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+         ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+         .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+         ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....
+         .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+         .#........#.#........#.#........#.#........#.#........#.#........#
+         #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+         #...##....##...##....##...##....##...##....##...##....##...##....#
+         .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"
+         };
+
+        assert_eq!(number_trees(input, slope), 2)
     }
 }
--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,12 +1,11 @@
 use std::fs;
 use std::path::Path;
-use day_3::number_trees;
+use day_3::{number_trees, Slope};
 
 
 fn main() {
     let contents = fs::read_to_string(Path::new("resources/input.txt"))
         .expect("Something went wrong reading the file");
 
-    // println!("{}", contents.as_str());
-    println!("Number of trees is: {}", number_trees(contents.as_str()));
+    println!("Number of trees is: {}", number_trees(contents.as_str(), Slope::new(1 , 2)));
 }
